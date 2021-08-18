c6cddf5623d05d74aa6bf2e45d2102badbda1963
rytis <rdereskevicius@gmail.com>
2021-08-18 19:15:40 +0300

    Implement PubSub consumer command

---
+++ src/Commands/PubSubConsume.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Kainxspirits\PubSubQueue\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class PubSubConsume extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'pubsub:consume
+                            {sub-name : The name of the sub to consume}
+                            {--sleep=3 : Number of seconds to sleep when no job is available}';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Start processing messages on the specified subscription';
+
+    /**
+     * @return void
+     */
+    public function handle(): void
+    {
+        $this->setSubscriptionToConsume($this->getSubscriptionName());
+
+        Artisan::call('queue:work', [
+            'connection' => 'pubsub',
+            '--sleep' => $this->getSleepOption()
+        ]);
+    }
+
+    /**
+     * @param string $subscriptionName
+     *
+     * @return void
+     */
+    private function setSubscriptionToConsume(string $subscriptionName): void
+    {
+        config(['queue.connections.pubsub.subscriber' => $subscriptionName]);
+    }
+
+    /**
+     * @return string
+     */
+    private function getSubscriptionName(): string
+    {
+        return $this->argument('sub-name');
+    }
+
+    /**
+     * @return string
+     */
+    private function getSleepOption(): string
+    {
+        return $this->option('sleep');
+    }
+}
--- src/PubSubQueueServiceProvider.php
+++ src/PubSubQueueServiceProvider.php
@@ -3,19 +3,40 @@
 namespace Kainxspirits\PubSubQueue;
 
 use Illuminate\Support\ServiceProvider;
+use Kainxspirits\PubSubQueue\Commands\PubSubConsume;
 use Kainxspirits\PubSubQueue\Connectors\PubSubConnector;
 
 class PubSubQueueServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
-     *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        $this->app['queue']->addConnector('pubsub', function () {
+        $this->registerPubSubCommands();
+        $this->addPubSubConnector();
+    }
+
+    /**
+     * @return void
+     */
+    private function addPubSubConnector(): void
+    {
+        $this->app['queue']->addConnector('pubsub', static function () {
             return new PubSubConnector;
         });
     }
+
+    /**
+     * @return void
+     */
+    private function registerPubSubCommands(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                PubSubConsume::class,
+            ]);
+        }
+    }
 }
+
